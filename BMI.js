e396434bde0889651e6ee3bcc76d28474cb12da7
alene <alene@ABDALLAH>
2025-02-11 14:43:45 +0300

    task

--- BMI.js
+++ BMI.js
@@ -8,7 +8,8 @@ function calculateBMI(weight, height) {
   /**
    *  Write your code here 👇🏼
    */
-  const BMI = 0;
+  const heightInMeters = height / 100;
+  const BMI = weight / Math.pow(heightInMeters, 2);
 
   /**
    * Stop making changes here!
@@ -27,6 +28,15 @@ function getBMIState(BMI) {
    */
   let BMIState;
 
+  if (BMI < 18.5) {
+    BMIState = "underweight";
+  } else if (BMI >= 18.5 && BMI <= 24.9) {
+    BMIState = "healthy";
+  } else if (BMI >= 25 && BMI <= 29.9) {
+    BMIState = "overweight";
+  } else {
+    BMIState = "obese";
+  }
   /**
    * Stop making changes here!
    */
@@ -43,7 +53,21 @@ function getBMIStateByAge(BMI, age) {
   /**
    *  Write your code here 👇🏼
    */
-  let BMIState = "not healthy";
+  if (age >= 19 && age <= 24 && BMI >= 19 && BMI <= 24) {
+    BMIState = "healthy";
+  } else if (age >= 25 && age <= 34 && BMI >= 20 && BMI <= 25) {
+    BMIState = "healthy";
+  } else if (age >= 35 && age <= 44 && BMI >= 21 && BMI <= 26) {
+    BMIState = "healthy";
+  } else if (age >= 45 && age <= 54 && BMI >= 22 && BMI <= 27) {
+    BMIState = "healthy";
+  } else if (age >= 55 && age <= 64 && BMI >= 23 && BMI <= 28) {
+    BMIState = "healthy";
+  } else if (age >= 65 && BMI >= 24 && BMI <= 29) {
+    BMIState = "healthy";
+  } else {
+    BMIState = "not healthy";
+  }
 
   /**
    * Stop making changes here!
